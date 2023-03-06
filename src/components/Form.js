f7b2f43557d8f611e4e420c037a73bfb84a8ef18
candide Mboungou-Kimpolo <mboungoukimpolo@gmail.com>
2023-03-06 13:50:37 -0600

    deployed app with aws

--- src/components/Form.js
+++ src/components/Form.js
@@ -2,72 +2,40 @@ import "./FormStyles.css";
 
 import React, { useState } from "react";
 
+const FORM_ENDPOINT =
+  "https://public.herotofu.com/v1/30ca8970-ac7e-11ed-a31e-753411848f80";
+
 const Form = () => {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const changeHandler = (event) => {
-    setFormState({
-      ...formState,
-      [event.target.name]: event.target.value,
-    });
+  const [submitted, setSubmitted] = useState(false);
+  const handleSubmit = () => {
+    setTimeout(() => {
+      setSubmitted(true);
+    }, 100);
   };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const config = {
-      username: "candide23@yopmail.com",
-      password: "E8E170B768548FDAC8299BFB4732D9304F7E",
-      host: "2525",
-      to: "candide23@yopmail.com",
-      from: formState.email,
-      subject: formState.subject,
-      body: `${formState.name}\n\n${formState.message}`,
-    };
-
-    if (window.Email) {
-      window.Email.send(config).then(() => {
-        alert("Email sent successfully");
-      });
-    }
-  };
+  if (submitted) {
+    return (
+      <>
+        <div className="text-2xl">Thank you!</div>
+        <div className="text-md">We'll be in touch soon.</div>
+      </>
+    );
+  }
 
   return (
     <div className="form">
-      <form onSubmit={submitHandler}>
-        <label>Your Name</label>
-        <input
-          type="text"
-          name="name"
-          value={formState.name}
-          onChange={changeHandler}
-        />
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formState.email}
-          onChange={changeHandler}
-        />
-        <label>Subject</label>
-        <input
-          type="text"
-          name="subject"
-          value={formState.subject}
-          onChange={changeHandler}
-        />
-        <label>Message</label>
-        <textarea
-          rows="6"
-          name="message"
-          value={formState.message}
-          onChange={changeHandler}
-        />
+      <form
+        action={FORM_ENDPOINT}
+        onSubmit={handleSubmit}
+        method="POST"
+        target="_blank"
+      >
+        <input type="text" placeholder="Your name" name="name" />
+
+        <input type="email" placeholder="Email" name="email" />
+
+        <textarea rows="6" placeholder="Your message" name="message" />
+
         <button className="btn">Submit</button>
       </form>
     </div>
@@ -75,93 +43,3 @@ const Form = () => {
 };
 
 export default Form;
-
-
-/*import React, { useState } from "react";
-
-const Form = () => {
-  const [formState, setFormState] = useState({
-    name: "",
-    email: "",
-    subject: "",
-    message: "",
-  });
-
-  const changeHandler = (event) => {
-    setFormState.name({
-      ...formState,
-      [event.target.name]: event.target.value,
-    });
-    setFormState.email({
-      ...formState,
-      [event.target.email]: event.target.value,
-    });
-    setFormState.subject({
-      ...formState,
-      [event.target.subject]: event.target.value,
-    });
-    setFormState.message({
-      ...formState,
-      [event.target.message]: event.target.value,
-    });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
-
-    const config = {
-      Username: "candide23@yopmail.com",
-      Password: "E8E170B768548FDAC8299BFB4732D9304F7E",
-      Host: "2525",
-      To: "candide23@yopmail.com",
-      From: formState.email,
-      Subject: "This is the subject",
-      Body: `${formState.name}`,
-    };
-
-    if (window.Email) {
-      window.Email.send(config).then(() => alert
-      ("email sent successfully")
-
-      );
-    }
-  };
-  return (
-    <div className="form" onSubmit={submitHandler}>
-      <form>
-        <label>Your Name</label>
-        <input
-          type="text"
-          name="name"
-          value={formState.name}
-          onChange={changeHandler}
-        ></input>
-        <label>Email</label>
-        <input
-          type="email"
-          name="email"
-          value={formState.email}
-          onChange={changeHandler}
-        ></input>
-        <label>Subject</label>
-        <input
-          type="text"
-          name="subject"
-          value={formState.subject}
-          onChange={changeHandler}
-        ></input>
-        <label>Message</label>
-        <input
-          type="text"
-          name="subject"
-          value={formState.message}
-          onChange={changeHandler}
-        ></input>
-        <textarea rows="6" placeholder="Type your message here" />
-        <button className="btn">Submit</button>
-      </form>
-    </div>
-  );
-};
-
-export default Form;*/
--- src/components/WorkCradData.js
+++ src/components/WorkCradData.js
@@ -7,20 +7,15 @@ const ProjectCardData = [
     imgsrc: pro1,
     title: "National Park Website",
     text: "My project is a group project where we create a website that can tell me all the information that I need about national Parks, so I can plan in advance my visit",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    view: "http://parknational.s3-website-us-east-1.amazonaws.com",
     source: "https://github.com/Candide23/Parks-N-Rec-Candide",
   },
   {
     imgsrc: pro2,
-    title: "LMS/Course Website Design",
+    title: "TrustHealthy",
     text: "Trusthealthy is an application which allows the user to view all the patient records, to register a new patient, to enter clinical data for a pateint, to analyze and see a report of the latest tests",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
-  },
-  {
-    imgsrc: pro3,
-    title: "Rymo Ecommerce Design",
-    text: "Top-ranked Thriving Facebook community and podcast, Amy inspires a grounded, tangible and self-affirming sense of “Wow! I really can do this” for over 100,000 online entrepreneurs. Best-selling marketing courses",
-    view: "https://www.youtube.com/watch?v=P8YuWEkTeuE",
+    view: "http://trusthealthy.s3-website-us-east-1.amazonaws.com/",
+    source: "https://github.com/Candide23/trusthealthy",
   },
 ];
 
